5defb25471073b1ae5d667f4b7aed259a0e0bdb3
Patrick Halim <90876112+pnhalim@users.noreply.github.com>
2023-07-26 00:40:29 -0400

    Time: 158 ms (25.68%), Space: 93.2 MB (73.46%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -2,23 +2,22 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         
-        auto buy_it = std::min_element(prices.begin(), prices.end());
-        auto sell_it = std::max_element(buy_it, prices.end());
+        int best_buy_price = prices[0];
+        int best_profit = 0;
         
-        int max_profit = *sell_it - *buy_it;
-        
-        while (buy_it != prices.begin()) {
-            auto old_buy_it = buy_it;
-            buy_it = std::min_element(prices.begin(), old_buy_it);
-            sell_it = std::max_element(buy_it, old_buy_it);
-            
-            int profit = *sell_it - *buy_it;
-            
-            if (profit > max_profit) {
-                max_profit = profit;
+        for (int i = 0; i < prices.size(); i++) {
+            if (best_buy_price > prices[i]) {
+                best_buy_price = prices[i];
+            }
+            else {
+                int profit = prices[i] - best_buy_price;
+                if (profit > best_profit) {
+                    best_profit = profit;
+                }
             }
         }
         
-        return max_profit;
+        return best_profit;
+        
     }
 };
