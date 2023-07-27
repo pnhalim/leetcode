51826516db41e720b6414d8254dbce34158099f6
Patrick Halim <90876112+pnhalim@users.noreply.github.com>
2023-07-27 18:10:52 -0400

    Time: 93 ms (99.41%), Space: 67.8 MB (33.23%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -2,12 +2,11 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         int max = nums[0];
-        int currSum = nums[0];
         
         for (int i = 1; i < nums.size(); i++) {
-            currSum = std::max(currSum + nums[i], nums[i]);
-            if (currSum > max) {
-                max = currSum;
+            nums[i] = std::max(nums[i-1] + nums[i], nums[i]);
+            if (nums[i] > max) {
+                max = nums[i];
             }
         }
         
