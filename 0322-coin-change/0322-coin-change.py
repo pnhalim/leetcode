33dfbfcfbc52491af4406c9f13cb20e3910542c6
Patrick Halim <90876112+pnhalim@users.noreply.github.com>
2023-09-25 08:31:41 +0700

    Time: 1536 ms (8.96%), Space: 47.3 MB (6.69%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -3,6 +3,11 @@ class Solution:
         memo = {}
         
         def recurse(m) -> int:
+            # dp
+            if m in memo.keys():
+                return memo[m]
+            
+            # base case
             if m == 0:
                 return 0
             elif m in coins:
@@ -10,15 +15,13 @@ class Solution:
             elif m < 0:
                 return float('inf')
             
-            if m in memo.keys():
-                return memo[m]
-            
             # try each coin
             min_count = float('inf')
             for c in coins:
-                memo[m-c] = recurse(m-c)
-                if memo[m-c] < min_count:
-                    min_count = memo[m-c]
+                if m-c > 0:
+                    memo[m-c] = recurse(m-c)
+                    if memo[m-c] < min_count:
+                        min_count = memo[m-c]
             if min_count == float('inf'):
                 return float('inf')
             return min_count + 1
