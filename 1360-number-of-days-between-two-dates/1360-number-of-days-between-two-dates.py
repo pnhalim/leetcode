5b25df94700a00ce35ab03d18323c84a1efc406f
Patrick Halim <90876112+pnhalim@users.noreply.github.com>
2023-09-27 01:29:06 -0400

    Time: 40 ms (55.36%), Space: 16.4 MB (53.33%) - LeetHub

--- 1360-number-of-days-between-two-dates/1360-number-of-days-between-two-dates.py
+++ 1360-number-of-days-between-two-dates/1360-number-of-days-between-two-dates.py
@@ -1,5 +1,5 @@
 class Solution:
     def daysBetweenDates(self, date1: str, date2: str) -> int:
-        d1 = datetime.date(year=int(date1[0:4]), month=int(date1[5:7]), day=int(date1[8:10]))
-        d2 = datetime.date(year=int(date2[0:4]), month=int(date2[5:7]), day=int(date2[8:10]))
+        d1 = datetime.datetime.strptime(date1, '%Y-%m-%d')
+        d2 = datetime.datetime.strptime(date2, '%Y-%m-%d')
         return int(abs(d2 - d1).days)
