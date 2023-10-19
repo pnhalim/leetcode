a6f09f758d22ae7060e06b6d0597f28aa8f68138
Patrick Halim <90876112+pnhalim@users.noreply.github.com>
2023-10-18 23:19:43 -0400

    Time: 41 ms (39.24%), Space: 16.4 MB (34.43%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,39 +1,28 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        ans = []
+
         if len(digits) == 0:
-            return ans
+            return []
         
-        def getLetters(digit: str):
-            if digit == '2':
-                return ('a', 'b', 'c')
-            elif digit == '3':
-                return ('d', 'e', 'f')
-            elif digit == '4':
-                return ('g', 'h', 'i')
-            elif digit == '5':
-                return ('j', 'k', 'l')
-            elif digit == '6':
-                return ('m', 'n', 'o')
-            elif digit == '7':
-                return ('p', 'q', 'r', 's')
-            elif digit == '8':
-                return ('t', 'u', 'v')
-            else:
-                return ('w', 'x', 'y', 'z')
-        
-        # initial fill with first letter
-        for letter in getLetters(digits[0]):
-            ans.append(letter)
-            
-        # bfs for all remaining letters
-        for digit in digits[1:]:
-            bfs = ans
-            ans = []
-            while len(bfs) > 0:
-                pre = bfs.pop()
-                new = getLetters(digit)
-                for letter in new:
-                    ans.append(pre + letter)
-        
+        letters = {'2': ['a','b','c'], '3': ['d','e','f'], '4': ['g','h','i'], 
+                   '5': ['j','k','l'], '6': ['m','n','o'], '7': ['p','q','r','s'],
+                   '8': ['t','u','v'], '9': ['w','x','y','z']}
+
+        dfs = []
+        dfs.append(("", digits))
+        ans = []
+
+        while dfs:
+            string, letter = dfs.pop()
+
+            if letter == "":
+                ans.append(string)
+                continue
+
+            curr, remaining = letter[0], letter[1:]
+            add_this = [(string + c, remaining) for c in letters[curr]]
+            print(add_this)
+            for elem in add_this:
+                dfs.append(elem)
+
         return ans
