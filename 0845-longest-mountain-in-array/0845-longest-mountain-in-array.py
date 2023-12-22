922e1c56cfa0a1a8d0f19afe6b78a083a78e3db2
Patrick Halim <90876112+pnhalim@users.noreply.github.com>
2023-12-21 19:26:15 -0500

    Time: 130 ms (99.78%), Space: 18.6 MB (5.62%) - LeetHub

--- 0845-longest-mountain-in-array/0845-longest-mountain-in-array.py
+++ 0845-longest-mountain-in-array/0845-longest-mountain-in-array.py
@@ -3,31 +3,44 @@ class Solution:
         
         longest = 0
         start = peak = end = 0
-
+        
+        [1,2,3,4,5,5,6,7,6,5,4,3]
+        
+        # find the correct start and ++ (must be decreasing)
         while start < len(arr) - 1:
             
-            # find start
             if arr[start] < arr[start + 1]:
-                # find peak
+                # find the peak 
                 peak = start
-                while peak < len(arr) - 1 and arr[peak] < arr[peak + 1]:
+                while peak < len(arr) - 1:
+                    if arr[peak] > arr[peak + 1]:
+                        break
+                    elif arr[peak] == arr[peak + 1]:
+                        start = peak
+                        break
                     peak += 1
-                # not a valid tree
-                if peak == start:
-                    start = max(start + 1, peak)
+                # check if peak is valid
+                if start == peak:
+                    start += 1
                     continue
 
-                # find end 
+                # find the end
                 end = peak
-                while end < len(arr) - 1 and arr[end] > arr[end + 1]:
+                while end < len(arr) - 1:
+                    if (arr[end] < arr[end + 1] 
+                        or arr[end] == arr[end + 1]):
+                        break
                     end += 1
-                if peak == end:
-                    start = max(start + 1, end)
-                    continue
 
-                # record the length
-                longest = max(longest, end - start + 1)
+                # record longest and start = end
+                if start != peak and peak != end:
+                    # valid mountain
+                    longest = max(longest, end - start + 1)
+                start = end
+                
+            else:
+                start += 1
             
-            start = max(start + 1, end)
-
         return longest
+        
+        
