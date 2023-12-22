e23ce5df9e139aed5560fb5cc54085ac303a22cb
Patrick Halim <90876112+pnhalim@users.noreply.github.com>
2023-12-21 19:09:35 -0500

    Time: 176 ms (81.98%), Space: 19 MB (7.56%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.py
+++ 0941-valid-mountain-array/0941-valid-mountain-array.py
@@ -1,19 +1,22 @@
 class Solution:
     def validMountainArray(self, arr: List[int]) -> bool:
+        
         if len(arr) < 3:
             return False
         
         peak = 0
         increasing = True
+        
         for i in range(0, len(arr) - 1):
+            if arr[i] == arr[i + 1]:
+                return False
+            
             if increasing:
-                if arr[i] > arr[i+1]:
-                    increasing = False
+                if arr[i] > arr[i + 1]:
                     peak = i
-                elif arr[i] == arr[i+1]:
-                    return False
-            elif not increasing:
-                if arr[i] < arr[i+1] or arr[i] == arr[i+1]:
+                    increasing = False
+            else:
+                if arr[i] < arr[i + 1]:
                     return False
                 
-        return peak != 0
+        return peak != 0 and peak != len(arr) - 1
