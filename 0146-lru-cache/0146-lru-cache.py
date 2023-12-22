0df7de7d12ab2dc64e2cae74dcc39df768e365e7
Patrick Halim <90876112+pnhalim@users.noreply.github.com>
2023-12-22 15:28:39 -0500

    Time: 575 ms (97.26%), Space: 78.7 MB (7.77%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,96 +1,124 @@
 class Node:
-    
-    def __init__(self, key: int, value: int):
+
+    def __init__(self, key: int, val: int):
         self.key = key
-        self.value = value
+        self.val = val
         self.prev = None
         self.next = None
 
-        
+
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
         self.size = 0
-        self.lru_head = None # this is the most recent
-        self.lru_tail = None # this is the least recent
-        self.data = {} # stores a node
+        self.data = {}          # <key, node(key, val)>
+        self.lru_head = None    # the most recently used
+        self.lru_tail = None    # the least recently used
         
-    def print_lru(self):
+    def print_lru(self) -> None:
+        print("printing lru...")
+        print(self.data)
+        # forward
         s = []
         n = self.lru_head
         while n:
-            s.append(str(n.value))
+            s.append(str(n.val))
             n = n.next
-        print(f"LRU Cache: {' '.join(s)}")
-        
+        print(f"lru: {' '.join(s)}")
+        # backward
         s = []
         n = self.lru_tail
         while n:
-            s.append(str(n.value))
+            s.append(str(n.val))
             n = n.prev
-        print(f"LRU Cache: {' '.join(s[::-1])}")
-        
-        print(self.data)
-        
+        print(f"lru: {' '.join(s[::-1])}")
+
     def update_lru(self, key: int) -> None:
+        """Updates the lru linked-list to put the key at the beginning."""
         node = self.data[key]
+        # remove node from list
         if node == self.lru_head:
             return
-        if node == self.lru_tail:
-            self.lru_tail = node.prev
-        
-        if node.next:
-            node.next.prev = node.prev
-        if node.prev:
-            node.prev.next = node.next
-        
-        if self.lru_head:
+        elif node == self.lru_tail:
+            self.lru_tail = self.lru_tail.prev
+            self.lru_tail.next = None
+        else:
+            prev = node.prev
+            next = node.next
+            if prev:
+                prev.next = node.next
+            if next:
+                next.prev = node.prev
+        node.prev = None
+        node.next = None
+        # add to beginning of lru
+        if not self.lru_head:
+            self.lru_head = node
+            self.lru_tail = node
+        else:
             self.lru_head.prev = node
             node.next = self.lru_head
-            node.prev = None
             self.lru_head = node
-        else:
-            self.lru_head = self.lru_tail = node
-        
-    def get(self, key: int) -> int:    
-        # print("GET")
+        # self.print_lru()
+
+
+    def get(self, key: int) -> int:
+        """Returns the val of key if exists. Else, returns -1."""
         if key in self.data:
             self.update_lru(key)
-            # self.print_lru()
-            return self.data[key].value
+            return self.data[key].val
         return -1
 
     def put(self, key: int, value: int) -> None:
+        """Updates or adds element to lru."""
         if key in self.data:
             self.update_lru(key)
-            self.data[key].value = value
+            self.data[key].val = value
         else:
-            node = Node(key, value)
-            self.data[key] = node
-            if self.lru_head:
-                self.lru_head.prev = node
-                node.next = self.lru_head
-                self.lru_head = node
-            else:
-                self.lru_head = self.lru_tail = node
-            self.size += 1
-            
-            if self.size > self.capacity:
-                key_to_delete = self.lru_tail.key
-                if self.lru_tail.prev:
-                    self.lru_tail.prev.next = None
-                self.lru_tail = self.lru_tail.prev
-                del self.data[key_to_delete]
-        
-#         print("PUT")
-#         self.print_lru()
-            
-            
-        
+            self.add(key, value)
+            # self.print_lru()
 
+    def add(self, key: int, value: int) -> None:
+        """Adds the node to the beginning of the lru."""
+        # add to dictionary
+        node = Node(key, value)
+        self.data[key] = node
+        self.size += 1        
+        # add node
+        if not self.lru_head:
+            self.lru_head = node
+            self.lru_tail = node
+        else:
+            self.lru_head.prev = node
+            node.next = self.lru_head
+            self.lru_head = node
+        # check for evict
+        if self.size > self.capacity:
+            self.evict()
+
+    def evict(self) -> None:
+        """Evicts the last element in the lru, pointed to by self.lru_tail."""
+        # remove from dictionary
+        key = self.lru_tail.key
+        del self.data[key]
+        # remove node
+        self.lru_tail = self.lru_tail.prev
+        self.lru_tail.next = None
+        self.size -= 1
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
 # obj.put(key,value)
+
+# lru = LRUCache(capacity=2)
+# lru.put(1, 1)
+# lru.put(2, 2)
+# lru.get(1)
+# lru.put(3, 3)
+# lru.get(2)
+# lru.put(4, 4)
+# lru.get(1)
+# lru.get(3)
+# lru.get(4)
