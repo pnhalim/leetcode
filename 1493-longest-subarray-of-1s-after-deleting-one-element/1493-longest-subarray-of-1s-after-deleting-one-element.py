46f5d2b18e1575b4504d0db83a0274267b028f94
Patrick Halim <90876112+pnhalim@users.noreply.github.com>
2024-02-12 12:22:14 -0500

    Time: 463 ms (61.72%), Space: 19.5 MB (94.78%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
@@ -1,30 +1,20 @@
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
         start = 0
-        mid = 0
         end = 0
-        max_len = 0
+        zero_count = 1 if nums[end] == 0 else 0
         
-        while end < len(nums):
-            if nums[start] == 0:
+        while end < len(nums) - 1:
+            end += 1
+            
+            if end < len(nums) and nums[end] == 0:
+                zero_count += 1
+                
+            if zero_count > 1:
+                if nums[start] == 0:
+                    zero_count -= 1
                 start += 1
-                mid = start
-                end = start
-            elif nums[mid] == 1:
-                mid += 1
-                end = mid + 1
-            elif nums[end] == 0:
-                max_len = max(max_len, end - 1 - start)
-                start = mid + 1
-                mid = mid + 1
-                end = mid + 1
-            elif nums[end] == 1:
-                end += 1
-
-        max_len = max(max_len, end - 1 - start)
-        while mid < len(nums) and nums[mid] == 1:
-            mid += 1
-        max_len = max(max_len, mid - start)
-        
-        
-        return max_len - 1 if (start == 0 and max_len == mid - start) else max_len
+                            
+        print(start, end, zero_count)
+        return end - start
+        # return (end - start) if (start != 0 or zero_count != 0) else (end - start - 1)
