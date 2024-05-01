e165d6994c69c95c8199d355009eeb8f7eb6b423
Patrick Halim <90876112+pnhalim@users.noreply.github.com>
2024-05-01 18:22:40 -0400

    Time: 102 ms (78.72%), Space: 17.3 MB (99.79%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,29 +1,20 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
+        # preprocess array, each elem stores the max index that it can go or anything before it can go
+        for i in range(1, len(nums)):
+            nums[i] = max(nums[i-1], i+nums[i])
+        
+        # iterate through array, go highest each time and inc count because highest jump is stored
         count = 0
-        
-        curr_index = len(nums) - 1
-        
-        while (curr_index > 0):
-            earliest_possible_jump = curr_index
-            
-            for i, n in enumerate(nums):
-                if (i + n >= curr_index):
-                    earliest_possible_jump = i
-                    break
-            
-            curr_index = earliest_possible_jump
+        i = 0
+        while (i < len(nums) - 1):
+            i = nums[i]
             count += 1
-            
+        
         return count
-     
+        
 # [2,3,1,1,4]
- 
-# [2,3,-,-,4]
-    
-    
-# [2,3,1,1,4]
-# [2,3,2,1,0]
 
-# [2,3,0,1,4]
-# [2,3,2,1,0]
+# [2,4,4,4,8]
+
+# [2,4]
