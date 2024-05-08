c9bce3c9f7b976fd09214f3563648593fa7f3948
Patrick Halim <90876112+pnhalim@users.noreply.github.com>
2024-05-07 20:19:47 -0400

    Time: 162 ms (48.87%), Space: 28.1 MB (35.62%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -4,18 +4,40 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         
-        # def swap(j, k):
-        #     temp = nums[j]
-        #     nums[j] = nums[k]
-        #     nums[k] = temp
+#         # Solution 1 - Temp Array
+#         # O(n) time, O(k) space
+#         k = k % len(nums)
+#         arr = nums[len(nums)-k:len(nums)]
+#         for i in range(len(nums)-1,k-1,-1):
+#             nums[i] = nums[i-k]
+#         for i in range(k):
+#             nums[i] = arr[i]
         
+#         # Solution 2 - Bubble k Times
+#         # O(nk) time, O(1) space
+#         def swap(i,j):
+#             temp = nums[i]
+#             nums[i] = nums[j]
+#             nums[j] = temp
+#         for _ in range(k):
+#             for i in range(len(nums)-1,0,-1):
+#                 swap(i, i-1)        
+    
+        # Solution 3 - Reverse
+        def reverse(start, end):
+            end -= 1
+            while (start < end):
+                temp = nums[start]
+                nums[start] = nums[end]
+                nums[end] = temp
+                start += 1
+                end -= 1
         k = k % len(nums)
-        arr = nums[len(nums)-k:len(nums)]
-        for i in range(len(nums)-1,k-1,-1):
-            nums[i] = nums[i-k]
-        for i in range(k):
-            nums[i] = arr[i]
-        
-        
+        reverse(0, len(nums))
+        reverse(0, k)
+        reverse(k, len(nums))
+    
 # [1,2,3,4,5,6,7]
+# [5,6,7,1,2,3,4]
+
 # [5,6,7,1,2,3,4]
