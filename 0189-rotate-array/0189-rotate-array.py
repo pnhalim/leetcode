ff2da5a0299d314be99bb378f1c838f47efb0977
Patrick Halim <90876112+pnhalim@users.noreply.github.com>
2024-05-07 20:21:13 -0400

    Time: 134 ms (99.83%), Space: 28.1 MB (10.16%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -4,14 +4,14 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         
-#         # Solution 1 - Temp Array
-#         # O(n) time, O(k) space
-#         k = k % len(nums)
-#         arr = nums[len(nums)-k:len(nums)]
-#         for i in range(len(nums)-1,k-1,-1):
-#             nums[i] = nums[i-k]
-#         for i in range(k):
-#             nums[i] = arr[i]
+        # Solution 1 - Temp Array
+        # O(n) time, O(k) space
+        k = k % len(nums)
+        arr = nums[len(nums)-k:len(nums)]
+        for i in range(len(nums)-1,k-1,-1):
+            nums[i] = nums[i-k]
+        for i in range(k):
+            nums[i] = arr[i]
         
 #         # Solution 2 - Bubble k Times
 #         # O(nk) time, O(1) space
@@ -23,22 +23,19 @@ class Solution:
 #             for i in range(len(nums)-1,0,-1):
 #                 swap(i, i-1)        
     
-        # Solution 3 - Reverse
-        # O(n) time, O(1) space
-        def reverse(start, end):
-            end -= 1
-            while (start < end):
-                temp = nums[start]
-                nums[start] = nums[end]
-                nums[end] = temp
-                start += 1
-                end -= 1
-        k = k % len(nums)
-        reverse(0, len(nums))
-        reverse(0, k)
-        reverse(k, len(nums))
-    
-# [1,2,3,4,5,6,7]
-# [5,6,7,1,2,3,4]
+#         # Solution 3 - Reverse
+#         # O(n) time, O(1) space
+#         def reverse(start, end):
+#             end -= 1
+#             while (start < end):
+#                 temp = nums[start]
+#                 nums[start] = nums[end]
+#                 nums[end] = temp
+#                 start += 1
+#                 end -= 1
+#         k = k % len(nums)
+#         reverse(0, len(nums))
+#         reverse(0, k)
+#         reverse(k, len(nums))
 
-# [5,6,7,1,2,3,4]
+        
