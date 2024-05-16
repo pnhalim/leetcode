a05d7577acf2c2fb02c0ae96d78f89e6a9aa1c76
Patrick Halim <90876112+pnhalim@users.noreply.github.com>
2024-05-16 19:04:45 -0400

    Time: 40 ms (53.03%), Space: 16.5 MB (98.39%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -2,23 +2,23 @@ class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
-        """
-        left = m-1
-        right = n-1
+        """     
+        m -= 1
+        n -= 1
         
-        while (left >= 0  and right >= 0):
-            if nums1[left] > nums2[right]:
-                nums1[left + right + 1] = nums1[left]
-                left -= 1
+        while (m >= 0  and n >= 0):
+            if nums1[m] > nums2[n]:
+                nums1[m + n + 1] = nums1[m]
+                m -= 1
             else:
-                nums1[left + right + 1] = nums2[right]
-                right -= 1
-        while (left >= 0):
-            nums1[left + right + 1] = nums1[left]
-            left -= 1
-        while (right >= 0):
-            nums1[left + right + 1] = nums2[right]
-            right -= 1
+                nums1[m + n + 1] = nums2[n]
+                n -= 1
+        while (m >= 0):
+            nums1[m + n + 1] = nums1[m]
+            m -= 1
+        while (n >= 0):
+            nums1[m + n + 1] = nums2[n]
+            n -= 1
             
             
         
