ca41d59892abc266e33a8662fa0beb45569156a4
Patrick Halim <90876112+pnhalim@users.noreply.github.com>
2024-05-16 19:02:41 -0400

    Time: 30 ms (97.01%), Space: 16.7 MB (24.93%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,22 +3,22 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        other_nums1 = nums1[:m]
-        left = 0
-        right = 0
-        while (left < m  and right < n):
-            if other_nums1[left] < nums2[right]:
-                nums1[left + right] = other_nums1[left]
-                left += 1
+        left = m-1
+        right = n-1
+        
+        while (left >= 0  and right >= 0):
+            if nums1[left] > nums2[right]:
+                nums1[left + right + 1] = nums1[left]
+                left -= 1
             else:
-                nums1[left + right] = nums2[right]
-                right += 1
-        while (left < m):
-            nums1[left + right] = other_nums1[left]
-            left += 1
-        while (right < n):
-            nums1[left + right] = nums2[right]
-            right += 1
+                nums1[left + right + 1] = nums2[right]
+                right -= 1
+        while (left >= 0):
+            nums1[left + right + 1] = nums1[left]
+            left -= 1
+        while (right >= 0):
+            nums1[left + right + 1] = nums2[right]
+            right -= 1
             
             
         
