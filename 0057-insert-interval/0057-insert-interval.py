92bf4f92daff1a78f0f5d2d392572612948dce4a
Patrick Halim <90876112+pnhalim@users.noreply.github.com>
2024-06-03 23:04:57 -0500

    Time: 75 ms (55.94%), Space: 19.8 MB (95.13%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -4,62 +4,32 @@ class Solution:
         if len(intervals) == 0:
             return [newInterval]
         
-        # insert the thing
-        inserted_at = 0
-        for i in range(len(intervals)-1):
-            if newInterval[0] >= intervals[i][0] and newInterval[0] <= intervals[i+1][0]:
-                # insert into this spot
-                intervals.insert(i+1, newInterval)
-                inserted_at = i+1
-                break
-        if inserted_at == 0:
-            if newInterval[0] < intervals[0][0]:
-                intervals.insert(0, newInterval)
-                inserted_at = 1
-            else:
-                intervals.append(newInterval)
-                inserted_at = len(intervals) - 1
-                
-        # print(intervals)
+        start = 0
+        end = 0
         
-        # continue merging until cannot merge
-        while (inserted_at < len(intervals) and intervals[inserted_at][0] <= intervals[inserted_at - 1][1]):
-            # merge 
-            intervals[inserted_at - 1][1] = max(intervals[inserted_at][1], intervals[inserted_at - 1][1])
-            intervals[inserted_at - 1][0] = min(intervals[inserted_at][0], intervals[inserted_at - 1][0])
-            intervals.pop(inserted_at)
+        for i in range(len(intervals)):
+            if newInterval[0] >= intervals[i][0]:
+                start = i
+            if newInterval[1] >= intervals[i][0]:
+                end = i
+
+        if newInterval[0] <= intervals[start][1]:
+            newInterval[0] = min(newInterval[0], intervals[start][0])
+            print(1)
+        else:
+            start += 1
+            print(2)
+        
+        if newInterval[1] >= intervals[end][0]:
+            newInterval[1] = max(newInterval[1], intervals[end][1])
+            print(3)
+        else:
+            end -= 1
+            print(4)
             
-        inserted_at += 1
-        
-        while (inserted_at < len(intervals) and intervals[inserted_at][0] <= intervals[inserted_at - 1][1]):
-            # merge 
-            intervals[inserted_at - 1][1] = max(intervals[inserted_at][1], intervals[inserted_at - 1][1])
-            intervals[inserted_at - 1][0] = min(intervals[inserted_at][0], intervals[inserted_at - 1][0])
-            intervals.pop(inserted_at)
-            
-        
-        return intervals
-        
-        
-        """
-        
-        Cases
-        
-        1 - insert and it doesn't matter
-        [[1,3],[6,9]] -> [>3, <6]
-        [[1,3],[4,5],[6,9]]
-        
-        2a - insert and it overlaps with one edge
-        [[1,3],[6,9]] -> [<=3,<6]
-        [[1,5],[6,9]]
-        
-        2b - insert and it overlaps with one edge
-        [[1,3],[6,9]] -> [>3,>=6]
-        [[1,3],[4,9]]
-        
-        3 - insert and it overlaps both edges 
-        [[1,3],[6,9]] -> [<=3,>=6]
-        [[1,9]]
-        
-        
-        """
+        result = intervals[0:start]
+        result.append(newInterval)
+        print(end + 1, len(intervals))
+        for i in range(end + 1, len(intervals)):
+            result.append(intervals[i])
+        return result
